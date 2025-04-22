51976908d10b420b26a82f0fae2012a320364789
Superinspired <bifernan@student.42.fr>
2025-04-23 00:22:02 +0200

    day00: ex04 finished

---
+++ day00/ex04/whatis.py
@@ -0,0 +1,28 @@
+import sys as sys
+
+
+def print_oddness(number: int):
+    if number % 2 == 0:
+        print("I'm Even.")
+    else:
+        print("I'm Odd.")
+
+
+def handler(number: str):
+    assert number.isnumeric() \
+            or (number[0] == "-"
+                and number[1:].isnumeric()), \
+            "argument is not an integer"
+    print_oddness(int(number))
+
+
+def main(argc: int, argv: list) -> int:
+    sys.tracebacklimit = 0
+    assert argc < 3, \
+        "more than one argument is provided"
+    if argc == 2:
+        handler(argv[1])
+    return 0
+
+
+main(len(sys.argv), sys.argv)
