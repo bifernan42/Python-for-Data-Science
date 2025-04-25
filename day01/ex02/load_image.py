810e02b4a0c13a7ec259f814cd2279c0ce46512c
Bilal Fernandes Placido <bifernan@made-f0Br6s10.clusters.42paris.fr>
2025-04-25 13:57:20 +0200

    day01: ex02

--- day01/ex02/load_image.py
+++ day01/ex02/load_image.py
@@ -1,14 +1,36 @@
 import numpy as np
 from pathlib import Path
 
-def ft_load(path: str) -> array:
+def try_and_except(condition: bool, message: str):
+    """
+        This function tries condition, prints message and exits if an error is raised
+    """
+    try:
+        assert condition, f"{message}"
+    except AsertionError as e:
+        print(f"AssertionError: {e}")
+        exit()
+
+def scan_image(image: file) -> None:
+    """
+        This function retrieves information
+        about the image and prints them
+    """
+
+
+
+def ft_load(path: str) -> np.ndarray:
     """
         This function loads an image, prints its format, and its pixels
         content in RGB format.
     """
     p = Path(path)
-    assert p.is_file(), f"File {path} does not exist"
+    try_and_except(p.is_file()), f"File {path} does not exist")
+    try:
+        with p.open() as image:
+            #traitement image
 
 
-    with p.open() as image: #traitment image
 
+    except Exception as e:
+        print("{e}: couldn't open file !")
