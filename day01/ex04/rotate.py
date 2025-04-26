3eec9ffe3dd6eb750e6034037cfc175a0ecf0d00
Superinspired <bifernan@student.42.fr>
2025-04-26 16:00:56 +0200

    day01: ex04 done

--- day01/ex04/rotate.py
+++ day01/ex04/rotate.py
@@ -2,15 +2,21 @@ import numpy as np
 from pathlib import Path
 import cv2
 import sys
+from load_image import ft_load
+import matplotlib.pyplot as plt
 
-def zoom_image(path: str, width: int, height: int):
+
+def rotate(path: str, width: int, height: int):
     image = ft_load(path)
-   
-
-
-
-    transposed = [image[j][i] for j in range()]
-    
+    print(len(image[0]))
+    print(len(image))
+    transposed = np.array([[image[j][i] for j in range(0, len(image))] for i in range(0, len(image[0]))])
     print(f"New shape after Transpose: {transposed.shape[:2]}")
-    imgplot = plt.imshow(zoomed_in) 
+    imgplot = plt.imshow(transposed) 
     plt.show()
+
+
+
+
+rotate("animal.jpeg", 0,0)
+
